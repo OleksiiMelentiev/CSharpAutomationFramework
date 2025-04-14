a46d7469c344191c5a203c56bc0bb3209d8cdf81
alexeymelentyev <alexey.melentyev@dataart.com>
2025-04-14 13:50:50 +0300

    add ui test base

---
+++ Framework.Ui/Tests/UiTestBase.cs
@@ -0,0 +1,47 @@
+using Framework.Api.Tests;
+using Framework.Ui.ExpectedResultsLists;
+using Framework.Ui.Helpers;
+using Framework.Ui.PagesLists;
+using Framework.Ui.TestDataManagersLists;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace Framework.Ui.Tests;
+
+public class UiTestBase : TestBase
+{
+    protected ErListUi Er { get; set; } = ErListUi.Get();
+    protected PagesList Pages { get; private set; } = PagesList.Get();
+    protected readonly TdmsListUi TestData = TdmsListUi.Get();
+
+    private PlaywrightHelper Playwright => PlaywrightFactory.Get();
+
+
+    [SetUp]
+    public async Task Setup()
+    {
+        await Playwright.CreateAsync();
+        await Playwright.StartTracingAsync();
+    }
+
+    [TearDown]
+    public async Task TearDown()
+    {
+        try
+        {
+            var tracingPath = await Playwright.StopTracing();
+
+            var isPassed = TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Passed;
+            if (isPassed == false)
+            {
+                var screenPath = await Playwright.TakeScreenshotAsync();
+                ExtentReports.LogScreenshot(screenPath);
+                ExtentReports.LogScreenshot(tracingPath, "tracing (open in a new tab to download)");
+            }
+        }
+        finally
+        {
+            await Playwright.CloseAsync();
+        }
+    }
+}
