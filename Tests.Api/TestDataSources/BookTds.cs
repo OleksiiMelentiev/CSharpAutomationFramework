84afbc21990d79e33a031ed46353fa55d3138a6b
xaldej <xaldej89@gmail.com>
2025-04-27 14:58:16 +0300

    add books tests

---
+++ Tests.Api/TestDataSources/BookTds.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace Tests.Api.TestDataSources;
+
+public static class BookTds
+{
+    public static IEnumerable<TestCaseData> Create()
+    {
+        yield return new TestCaseData(true);
+        yield return new TestCaseData(false);
+    }
+}
