30903e48f00e40f80c625e3f0a9e3a5e71ba93c3
xaldej <xaldej89@gmail.com>
2025-04-27 14:33:38 +0300

    add book mock

--- Framework.Api/MocksLists/MocksList.cs
+++ Framework.Api/MocksLists/MocksList.cs
@@ -5,6 +5,7 @@ namespace Framework.Api.MocksLists;
 public class MocksList : IDisposable
 {
     private readonly PersonMock _person = new();
+    private readonly BookMock _book = new();
 
 
     private static MocksList? _instance;
@@ -18,12 +19,15 @@ public class MocksList : IDisposable
     public void Run()
     {
         _person.RunServer();
+        _book.RunServer();
         
         _person.SetMocks();
+        _book.SetMocks();
     }
 
     public void Dispose()
     {
         _person.StopServer();
+        _book.StopServer();
     }
 }
---
+++ Mocks/BookMock.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+using Common.Helpers;
+using Models;
+using Models.Requests.Book;
+using WireMock;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Types;
+using WireMock.Util;
+
+namespace Mocks;
+
+public class BookMock : MockBase
+{
+    private static readonly IList<Book> BookDbSimulation = new List<Book>();
+
+    public void SetMocks()
+    {
+        MockCreateBook();
+        MockGetBook();
+    }
+
+
+    private void MockGetBook()
+    {
+        Server?.Given(
+                Request.Create()
+                    .WithPath("/api/book/*")
+                    .UsingGet()
+            )
+            .RespondWith(Response.Create().WithCallback(request =>
+            {
+                var pathSegments = request.Path.Split('/');
+                var id = Guid.Parse(pathSegments.Last());
+
+                var book = BookDbSimulation.FirstOrDefault(b => b.Id == id);
+
+                var statusCode = book == null ? 404 : 200;
+                var responseJson = book == null ? string.Empty : JsonHelper.Serialize(book);
+
+                return new ResponseMessage()
+                {
+                    StatusCode = statusCode,
+                    BodyData = new BodyData
+                    {
+                        Encoding = Encoding.UTF8,
+                        DetectedBodyType = BodyType.String,
+                        BodyAsString = responseJson,
+                    },
+                };
+            }));
+    }
+
+    private void MockCreateBook()
+    {
+        Server?.Given(
+                Request.Create()
+                    .WithPath("/api/book")
+                    .UsingPost()
+            )
+            .RespondWith(Response.Create().WithCallback(request =>
+            {
+                var requestJson = request.BodyAsJson?.ToString() ?? string.Empty;
+                var requestBody = JsonHelper.Deserialize<CreateBookRequest>(requestJson);
+                var book = new Book()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = requestBody.Title,
+                    Author = requestBody.Author,
+                    PublicationDate = requestBody.PublicationDate,
+                };
+                BookDbSimulation.Add(book);
+
+                return new ResponseMessage()
+                {
+                    StatusCode = 200,
+                    BodyData = new BodyData
+                    {
+                        Encoding = Encoding.UTF8,
+                        DetectedBodyType = BodyType.String,
+                        BodyAsString = JsonHelper.Serialize(book),
+                    },
+                };
+            }));
+    }
+}
